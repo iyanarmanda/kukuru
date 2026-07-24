97bd48ae15393ffa9a0d8646e2c97dfb7aa8d93d
Iyan Z Armanda <150656877+iyanarmanda@users.noreply.github.com>
2026-07-24 14:11:23 +0700

    fix: support i18n if web have base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,7 +33,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://iyanarmanda.github.io/",
-	base: "/",
+	base: "/kukuru",
 	integrations: [
 		swup({
 			theme: false,
