26c920b090815e99d60517b3f5a97759cd9b85ec
Iyan Z Armanda <150656877+iyanarmanda@users.noreply.github.com>
2026-08-05 15:59:06 +0700

    perf: simplified rehype mermaid config and style

--- src/plugins/rehype-mermaid.mjs
+++ src/plugins/rehype-mermaid.mjs
@@ -1,23 +1,8 @@
-import { fileURLToPath } from "node:url";
 import rehypeMermaid from "rehype-mermaid";
-import * as sass from "sass";
-
-const mermaidTheme = fileURLToPath(
-	new URL("../styles/mermaid.scss", import.meta.url),
-);
-
-const compiledCss = sass.compile(mermaidTheme).css;
 
 export const rehypeMermaidConfig = [
 	rehypeMermaid,
 	{
 		strategy: "inline-svg",
-		mermaidConfig: {
-			theme: "base",
-			themeVariables: {
-				background: "transparent",
-			},
-			themeCSS: compiledCss,
-		},
 	},
 ];
